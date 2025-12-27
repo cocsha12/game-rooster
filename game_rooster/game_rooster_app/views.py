508ae98a30015c93b96081df4dabac946b20b3ba
Your Name <you@example.com>
2025-12-27 11:50:01 +0300

    fgfgfgfgf

--- game_rooster/game_rooster_app/views.py
+++ game_rooster/game_rooster_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import JsonResponse
 
 def index(request):
     return render(request, 'index.html')
@@ -15,6 +17,22 @@ def auth(request):
     return render(request, 'auth.html')
 
 def reg(request):
+
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        first_name = request.POST.get('firstName')
+        last_name = request.POST.get('lastName')
+        birthday = request.POST.get('birthday')
+        username = email
+
+        print('Почта: ', email, '\n', 'Пароль: ', password, 'Имя: ', first_name, sep='')
+
+        user = User.objects.create_user(username, email, password)
+
+        return JsonResponse({'status': 'success', 'message': 'Everything OK'})
+
+
     return render(request, 'reg.html')
 
     
